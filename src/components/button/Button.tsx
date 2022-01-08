8c9dc585f03fbf41b4b550b0f26a9e749df205c3
claud-io <cbiselli89@gmail.com>
2022-01-08 21:02:35 +0100

    button changes

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -10,7 +10,7 @@ export const typeClasses: ButtonClasses<ButtonType> = {
 
 export const sizeClasses: ButtonClasses<ButtonSize> = {
   Small: "px-1.5 py-1 text-sm font-medium",
-  Base: "px-3 py-1.5 text-sm font-medium",
+  Base: "px-3 py-1.5 text-sm font-medium ",
   Large: "px-4 py-2 text-base font-medium",
 };
 
